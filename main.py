23d7f4236bb847053d5da5f01a09817a49ffacd8
sneya-gabreate <sneya.gabreate@perficient.com>
2026-06-04 10:33:52 +0530

    merged

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 def add(x,y):
     return x+y
 print(add(2,3))
+
+def subtract(x,y):
+    return x-y  
+print(subtract(5,2))
+def multiply(x,y):
+    return x*y      
+print(multiply(4,6))
+
+
+def divide(x,y):
+    if y != 0:
+        return x/y
+    else:
+        return "Cannot divide by zero"
+print(divide(10,2))
+print(divide(10,0))
